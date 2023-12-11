edd2262d54941ea3a38235737f333200a3b38d23
Ryo390 <151626117+Ryo390@users.noreply.github.com>
2023-12-11 17:34:19 +0900

    新規投稿機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :prototypes, only:[:index ,:new]
+  resources :prototypes, only:[:index ,:new,:create]
    root to: "prototypes#index"
   end
