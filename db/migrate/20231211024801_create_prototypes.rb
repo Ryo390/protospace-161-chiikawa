9812d1137b011974380c639f0e89abfa2c609636
Ryo390 <151626117+Ryo390@users.noreply.github.com>
2023-12-11 16:06:38 +0900

    投稿ページの作成

--- db/migrate/20231211024801_create_prototypes.rb
+++ db/migrate/20231211024801_create_prototypes.rb
@@ -1,7 +1,7 @@
 class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
     create_table :prototypes do |t|
-      t.refarences :user,null: false,foreign_key: true
+      t.references :user,null: false,foreign_key: true
       t.string :name,null: false
       t.string :concept,null: false
       t.string :catch_copy,null: false
