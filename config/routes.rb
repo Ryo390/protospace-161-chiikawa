06abd23abb138a6451921cc1ebfd3ef826033650
Ryo390 <151626117+Ryo390@users.noreply.github.com>
2023-12-12 21:44:16 +0900

    コメントのDBへの登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :prototypes
-  resources :users, only: :show
   root to: "prototypes#index"
+  resources :prototypes do
+  resources :comments, only: :create
+  end
+  resources :users, only: :show
 end
