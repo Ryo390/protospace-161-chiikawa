ac837180b7e120ca16956dbf24e0a1905ac90fc2
argonsys <kenon12552@icloud.com>
2023-12-12 17:56:06 +0900

    prototypes#destroyのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :prototypes, only:[:index, :new, :create, :show, :edit, :update]
+  resources :prototypes
   resources :users, only: :show
   root to: "prototypes#index"
 end
